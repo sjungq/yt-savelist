5e45c80fa011e445ff13052fa5957f70fbb51826
Stacy Jung <stacyj1417@gmail.com>
2021-10-10 15:15:42 -0700

    Styling redone for playlist lookup

--- public/scripts/backup.js
+++ public/scripts/backup.js
@@ -1,4 +1,10 @@
 //probably where we generate the requested .txt/.json file
 //console.log("im here!");
 
-function generatePlaylistFile() {}
+function generatePlaylistFile() {
+  console.log("test from backup.js");
+}
+
+document
+  .querySelector("#txtSaveBtn")
+  .addEventListener("click", generatePlaylistFile);
